eb76874f568c14a9a21b905a448e3e02095a3200
Vasiliy Lezhnin <vasiliylezhnin@MacBook-Pro-Vasiliy.local>
2026-07-10 22:58:51 +0500

    task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,40 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return arr;
+  }
+
+  if (d === 0) {
+    const result = -b / (2 * a);
+    arr.push(result);
+    return arr;
+  }
+
+  if (d > 0) {
+    const result1 = (-b + Math.sqrt(d)) / (2 * a);
+    const result2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(result1, result2);
+    return arr;
+  }
 }
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthRate = percent / 1200;
+  let credit = amount - contribution;
+  let p = monthRate;
+  let n = countMonths;
+  let s = credit;
+
+  let denominator = ((1 + p) ** n) - 1;
+  let total = s * (p + p / denominator);
+  let totalAmount = total * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
