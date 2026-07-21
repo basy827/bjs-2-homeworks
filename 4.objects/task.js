5b48ff19f21926f5fb01ffc03c9d12d11c606abf
basy827 <basy827@gmail.com>
2026-07-21 13:26:41 +0500

    task-4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+  this.marks = [];
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.excluded || this.marks === undefined) {
+    return;
+  }
+  this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  } else {
+    return this.marks.reduce((sum, mark) => sum + mark) / this.marks.length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
+  console.log("Плохая учеба")
 }
